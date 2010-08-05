4193f7b235ce7f575b3b7baa4cabcb5b00a67a6c
rdohms <rdohms@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-05 12:15:23 +0000

    Merge branch '0.2' into wp-trunk
    git-svn-id: https://plugins.svn.wordpress.org/tweester/trunk@271974 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tweester.php
+++ tweester.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Tweester
-Plugin URI: --
+Plugin URI: http://github.com/rdohms/Tweester
 Description: This plugin allows you to show a page of people who supported your cause on twitter.
-Version: 0.2
+Version: 0.2.1
 Author: Rafael Dohms
 Author URI: http://www.rafaeldohms.com.br
 License: CC
